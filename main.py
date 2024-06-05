32723c7c12d735cafd6fdb8341efba8ec34048ce
Raphael B. Dias <86736451+raphaelbdias@users.noreply.github.com>
2024-06-05 13:48:59 +0000

    base setup

--- main.py
+++ main.py
@@ -1,12 +1,21 @@
-from flask import Flask
+from flask import Flask, render_template
 import pandas as pd
 
 app = Flask(__name__)
 
+
+# Analysis (for example purposes, you can add more analyses as needed)
+def get_summary_statistics():
+    return df.describe()
+
+def get_exit_analysis():
+    return df.groupby('Exited').size()
+
 @app.route('/')
 def index():
-    return 'Hello, World!'
+    # summary_statistics = get_summary_statistics().to_html()
+    # exit_analysis = get_exit_analysis().to_frame().to_html()
+    return render_template('index.html')
 
 if __name__ == '__main__':
     app.run(debug=True)
-
